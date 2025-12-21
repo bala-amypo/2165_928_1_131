061360b9fc2b3d7b40a19cd7e0fa845b8c09daa4
bala-amypo <bala@amypo.in>
2025-12-21 06:22:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/LoadSheddingEventRepository.java
+++ src/main/java/com/example/demo/repository/LoadSheddingEventRepository.java
@@ -1,22 +1,11 @@
 package com.example.demo.repository;
 
-
-
 import com.example.demo.entity.LoadSheddingEvent;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 
 import java.util.List;
 
-
-
 public interface LoadSheddingEventRepository extends JpaRepository<LoadSheddingEvent, Long> {
 
-
-
-List<LoadSheddingEvent> findByZoneIdOrderByEventStartDesc(Long zoneId);
-
+    List<LoadSheddingEvent> findByZoneIdOrderByEventStartDesc(Long zoneId);
 }
-
