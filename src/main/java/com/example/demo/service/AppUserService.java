9981f2b14c101042d943adeefc6541eceabf0926
bala-amypo <bala@amypo.in>
2025-12-24 15:36:16 +0000

    User commit

--- src/main/java/com/example/demo/service/AppUserService.java
+++ src/main/java/com/example/demo/service/AppUserService.java
@@ -1,11 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.AuthResponse;
 import com.example.demo.entity.AppUser;
 
 public interface AppUserService {
-
-    AppUser register(String email, String password, String role);
-
-    AuthResponse login(String email, String password);
+    AppUser createUser(String email, String password);
+    AppUser getByEmail(String email);
 }
