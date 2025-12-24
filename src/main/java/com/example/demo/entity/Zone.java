dade32d563b649a4f1582204fdaac3b6961d36d5
bala-amypo <bala@amypo.in>
2025-12-24 15:09:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/Zone.java
+++ src/main/java/com/example/demo/entity/Zone.java
@@ -1,53 +1,19 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.Instant;
 
-@Entity
-@Table(
-        name = "zones",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = "zoneName")
-        }
-)
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Zone {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false, unique = true)
     private String zoneName;
-
-    @Column(nullable = false)
     private Integer priorityLevel;
-
     private Integer population;
-
-    @Builder.Default
-    private Boolean active = true;
-
-    private Instant createdAt;
+    private Boolean active;
     private Instant updatedAt;
-
-    @PrePersist
-    public void onCreate() {
-        this.createdAt = Instant.now();
-        this.updatedAt = Instant.now();
-        if (this.active == null) {
-            this.active = true;
-        }
-    }
-
-    @PreUpdate
-    public void onUpdate() {
-        this.updatedAt = Instant.now();
-    }
 }
