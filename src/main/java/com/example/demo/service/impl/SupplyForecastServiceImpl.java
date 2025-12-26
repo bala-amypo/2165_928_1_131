ad397bd34fe365130ca9411172ba0ea1f04d115c
bala-amypo <bala@amypo.in>
2025-12-26 06:20:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplyForecastServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplyForecastServiceImpl.java
@@ -1,13 +1,17 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.SupplyForecast;
-import com.example.demo.exception.*;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.SupplyForecastRepository;
 import com.example.demo.service.SupplyForecastService;
+
+import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.List;
 
+@Service
 public class SupplyForecastServiceImpl implements SupplyForecastService {
 
     private final SupplyForecastRepository repo;
