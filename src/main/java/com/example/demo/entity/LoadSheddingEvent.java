eea2a0282be915b39bb4857d0f5ae68754fda265
bala-amypo <bala@amypo.in>
2025-12-26 05:26:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoadSheddingEvent.java
+++ src/main/java/com/example/demo/entity/LoadSheddingEvent.java
@@ -1,8 +1,12 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+
 import java.time.Instant;
 
+@Entity
+@Table(name = "load_shedding_events")
 @Getter
 @Setter
 @Builder
@@ -10,9 +14,17 @@ import java.time.Instant;
 @AllArgsConstructor
 public class LoadSheddingEvent {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long zoneId;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "zone_id")
+    private Zone zone;
+
     private Double expectedDemandReductionMW;
+
     private Instant eventStart;
+
     private String reason;
 }
